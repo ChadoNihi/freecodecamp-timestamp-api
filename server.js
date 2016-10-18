444df46dcebe9704031fef5a230e1d84fa4f53ca
Alexandr Shchelov <chadoishere@gmail.com>
2016-10-18 17:30:53 +0000

    before testing

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express'),
+      port = 8080,
       app = express();
       
+app.set('view engine', 'pug');
+      
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.render('index', { fullUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
+                            title: 'Instructions | Timestamp App'});
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:timeStr', function (req, res) {
+    var date = new Date(req.params.timeStr),
+        result;
+    
+    if (Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date.getTime())) {
+        result = {
+            dateString: date.toDateString(),
+            unixTime: date.getTime()
+        };
+    } else {
+        //res.statusCode = 400;
+        result = {};
+    }
+    
+    res.send(result);
+});
+
+app.listen(port, function () {
+  console.log('The timestamp app is listening on port '+port+'!');
 });
